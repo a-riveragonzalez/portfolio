1ecb483769caeec475818a04f39f3dcc85a2f1c2
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-10-24 23:37:00 -0700

    fixed carousel

--- src/components/pages/Portfolio/index.js
+++ src/components/pages/Portfolio/index.js
@@ -1,100 +1,52 @@
 import React from "react";
+import Carousel from "react-bootstrap/Carousel";
 import "./portfolio.css";
 
 export default function Portfolio() {
   return (
-    <div className="portfolio">
-      <h1>Portfolio Page</h1>
+    <Carousel fade>
+      {/* ********************* Item 1 ************************ */}
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="./assets/impulsescreenshot.png"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
 
-      {/* ************************* Carousel ***************************** */}
-      <div
-        id="carouselExampleCaptions"
-        className="carousel slide"
-        data-ride="carousel"
-      >
-        <ol className="carousel-indicators">
-          <li
-            data-target="#carouselExampleCaptions"
-            data-slide-to="0"
-            className="active"
-          ></li>
-          <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-          <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-        </ol>
+      {/* ********************* Item 2 ************************ */}
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="./assets/event-fan-follow.gif"
+          alt="Second slide"
+        />
 
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img
-              src="./assets/impulsescreenshot.png"
-              className="d-block w-100"
-              alt="..."
-            >
-              
-            </img>
-            <div className="carousel-caption d-none d-md-block">
-              <h5>First slide label</h5>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </div>
-          </div>
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
 
-          <div className="carousel-item">
-            <img
-              src="https://aasports89.github.io/Event-Fan-Follow/"
-              className="d-block w-100"
-              alt="..."
-            >
-              
-            </img>
-            <div className="carousel-caption d-none d-md-block">
-              <h5>Second slide label</h5>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-          </div>
+      {/* ********************* Item 3 ************************ */}
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="./assets/1weather.png"
+          alt="Third slide"
+        />
 
-          <div className="carousel-item">
-            <img
-              src="https://a-riveragonzalez.github.io/weather-dashboard/"
-              className="d-block w-100"
-              alt="..."
-            >
-              
-            </img>
-            <div className="carousel-caption d-none d-md-block">
-              <h5>Third slide label</h5>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <a
-          className="carousel-control-prev"
-          href="#carouselExampleCaptions"
-          role="button"
-          data-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="sr-only">Previous</span>
-        </a>
-        <a
-          className="carousel-control-next"
-          href="#carouselExampleCaptions"
-          role="button"
-          data-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="sr-only">Next</span>
-        </a>
-      </div>
-
-    
-    </div>
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 }
