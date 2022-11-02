1836bd473467153a26d82899b76647d07795d96a
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-11-01 22:54:30 -0700

    added three more slides on portfolio

--- src/components/pages/Portfolio/index.js
+++ src/components/pages/Portfolio/index.js
@@ -4,17 +4,21 @@ import "./portfolio.css";
 
 export default function Portfolio() {
   return (
-    <Carousel fade className="portfolio">
+    <Carousel fade>
       {/* ********************* Item 1 ************************ */}
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="./assets/impulsescreenshot.png"
+          src="./assets/poke-battle-2.png"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h3>I Choose You</h3>
+          <p>
+            A responsive MERN dating web application that allows users to create
+            Pokemon-themed profiles, take a quiz to determine their Pokemon, and
+            match with other users to chat.
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
 
@@ -22,12 +26,18 @@ export default function Portfolio() {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="./assets/event-fan-follow.gif"
+          src="./assets/impulsescreenshot.png"
           alt="Second slide"
         />
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h3>Impulse: The Game</h3>
+          <p>
+            {" "}
+            A full stack browser-board-game where your character advances
+            through game squares upon which they are compelled to Buy an Item or
+            choose a Life Event Card that will impact your total Money and Mood
+            Levels.
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
 
@@ -35,13 +45,68 @@ export default function Portfolio() {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="./assets/1weather.png"
+          src="./assets/event-fan-follow.gif"
           alt="Third slide"
         />
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <h3>Event Fan Follow</h3>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            Event-Fan-Follow is a web-based application utilizing HTML, CSS,
+            JavaScript, JQuery, TicketMaster, and Google Maps APIs. This
+            application centers on retrieving and processing data from the API
+            URLs, making it a user-friendly method to simultaneously display
+            event ticket information combined with geolocation directly on the
+            client side.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      {/* ********************* Item 4 ************************ */}
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="./assets/techblog-homepage.png"
+          alt="Fourth slide"
+        />
+        <Carousel.Caption>
+          <h3>Tech Blog</h3>
+          <p>
+            A web application,, similar to a Wordpress site, where developers
+            can publish their blog posts and comment on other developers posts
+            as well.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      {/* ********************* Item 5 ************************ */}
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="./assets/1weather.png"
+          alt="Fifth slide"
+        />
+        <Carousel.Caption>
+          <h3>Weather Dashboard</h3>
+          <p>
+            A weather dashboard that allows users to search for a city and get
+            its current weather and a five day forecast.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      {/* ********************* Item 6 ************************ */}
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="./assets/note-taker.gif"
+          alt="Sixth slide"
+        />
+        <Carousel.Caption>
+          <h3>Note Taker</h3>
+          <p>
+            A web application,, similar to a Wordpress site, where developers
+            can publish their blog posts and comment on other developers posts
+            as well.
           </p>
         </Carousel.Caption>
       </Carousel.Item>
