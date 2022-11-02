613d03999b1c023fe38a9f1ae2ee6e30ea35ef1e
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-11-02 15:01:35 -0700

    added tools used to portfolio and links

--- src/components/pages/Portfolio/index.js
+++ src/components/pages/Portfolio/index.js
@@ -7,11 +7,13 @@ export default function Portfolio() {
     <Carousel fade>
       {/* ********************* Item 1 ************************ */}
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="./assets/poke-battle-2.png"
-          alt="First slide"
-        />
+        <a href="https://ichooseyou.herokuapp.com">
+          <img
+            className="d-block w-100"
+            src="./assets/poke-battle-2.png"
+            alt="First slide"
+          />
+        </a>
         <Carousel.Caption>
           <h3>I Choose You</h3>
           <p>
@@ -19,16 +21,27 @@ export default function Portfolio() {
             Pokemon-themed profiles, take a quiz to determine their Pokemon, and
             match with other users to chat.
           </p>
+          <p>
+            <strong>Tools Used : </strong>Javascript, React, GraphQL, Node.js,
+            Express.js, MongoDB, Mongoose ODM, JWT, CSS, Bootstrap
+          </p>
+          <p>
+            GitHub :{" "}
+            <a
+              href="https://github.com/a-riveragonzalez/i-choose-you"
+              className="fa-solid fa-code"
+            > </a>
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
 
       {/* ********************* Item 2 ************************ */}
       <Carousel.Item>
-        <img
+      <a href="https://impulse-the-game.herokuapp.com/"><img
           className="d-block w-100"
           src="./assets/impulsescreenshot.png"
           alt="Second slide"
-        />
+        /></a>
         <Carousel.Caption>
           <h3>Impulse: The Game</h3>
           <p>
@@ -38,16 +51,28 @@ export default function Portfolio() {
             choose a Life Event Card that will impact your total Money and Mood
             Levels.
           </p>
+          <p>
+            <strong>Tools Used : </strong>JavaScript, Node.js, Express, Sequelize, Handlebars, MVC
+                structure, Cloudinary, Bcrypt, .ENV, jQuery, Bootstrap, HTML,
+                CSS, Google Fonts
+          </p>
+          <p>
+            GitHub :{" "}
+            <a
+              href="https://github.com/LujanSolo/Impulse"
+              className="fa-solid fa-code"
+            > </a>
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
 
       {/* ********************* Item 3 ************************ */}
       <Carousel.Item>
-        <img
+      <a href="https://aasports89.github.io/Event-Fan-Follow/"><img
           className="d-block w-100"
           src="./assets/event-fan-follow.gif"
           alt="Third slide"
-        />
+        /></a>
         <Carousel.Caption>
           <h3>Event Fan Follow</h3>
           <p>
@@ -58,55 +83,95 @@ export default function Portfolio() {
             event ticket information combined with geolocation directly on the
             client side.
           </p>
+          <p>
+            <strong>Tools Used : </strong>HTML, CSS, JavaScript, jQuery, Bootstrap, MomentJS, Google
+                Fonts, TicketMaster API, Google Maps API
+          </p>
+          <p>
+            GitHub :{" "}
+            <a
+              href="https://github.com/AASports89/Event-Fan-Follow"
+              className="fa-solid fa-code"
+            > </a>
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
 
       {/* ********************* Item 4 ************************ */}
       <Carousel.Item>
-        <img
+      <a href="https://tech-blog-6.herokuapp.com/"><img
           className="d-block w-100"
           src="./assets/techblog-homepage.png"
           alt="Fourth slide"
-        />
+        /></a>
         <Carousel.Caption>
           <h3>Tech Blog</h3>
           <p>
-            A web application,, similar to a Wordpress site, where developers
+            A web application, similar to a Wordpress site, where developers
             can publish their blog posts and comment on other developers posts
             as well.
+          </p>
+          <p>
+            <strong>Tools Used : </strong>JavaScript, CSS, MVC, MySql, Express.js, Node.js, Sequelize, Handlebars, Bootstrap, Google Fonts, and bcrypt
+          </p>
+          <p>
+            GitHub :{" "}
+            <a
+              href="https://github.com/a-riveragonzalez/tech-blog"
+              className="fa-solid fa-code"
+            > </a>
           </p>
         </Carousel.Caption>
       </Carousel.Item>
 
       {/* ********************* Item 5 ************************ */}
       <Carousel.Item>
-        <img
+      <a href="https://a-riveragonzalez.github.io/weather-dashboard/"><img
           className="d-block w-100"
           src="./assets/1weather.png"
           alt="Fifth slide"
-        />
+        /></a>
         <Carousel.Caption>
           <h3>Weather Dashboard</h3>
           <p>
             A weather dashboard that allows users to search for a city and get
             its current weather and a five day forecast.
           </p>
+          <p>
+            <strong>Tools Used : </strong>HTML, CSS, JavaScript, jQuery, Bootstrap, MomentJS, Google
+                Fonts, OpenWeather API
+          </p>
+          <p>
+            GitHub :{" "}
+            <a
+              href="https://github.com/a-riveragonzalez/weather-dashboard"
+              className="fa-solid fa-code"
+            > </a>
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
 
-      {/* ********************* Item 6 ************************ */}
+      {/* ********************* TODO Item 6 ************************ */}
       <Carousel.Item>
-        <img
+      <a href="https://rocky-headland-83783.herokuapp.com/"><img
           className="d-block w-100"
           src="./assets/note-taker.gif"
           alt="Sixth slide"
-        />
+        /></a>
         <Carousel.Caption>
           <h3>Note Taker</h3>
           <p>
-            A web application,, similar to a Wordpress site, where developers
-            can publish their blog posts and comment on other developers posts
-            as well.
+          An application that can be used to write, save, and delete notes.
+          </p>
+          <p>
+            <strong>Tools Used : </strong>Express.js, JavaScript, HTML, CSS, Bootstrap, and FontAwesome
+          </p>
+          <p>
+            GitHub :{" "}
+            <a
+              href="https://github.com/a-riveragonzalez/note-taker"
+              className="fa-solid fa-code"
+            > </a>
           </p>
         </Carousel.Caption>
       </Carousel.Item>
--- src/components/pages/Resume/index.js
+++ src/components/pages/Resume/index.js
@@ -4,14 +4,19 @@ import "./resume.css";
 export default function Resume() {
   return (
     <div className="resume text-center">
-      <h1 >Resume</h1>
+      <h1>Resume</h1>
 
       {/* todo : Link to download resume  */}
-      <button type="button" className="btn btn-primary btn-block w-50">Download my resume</button>
+      <a
+        href="https://drive.google.com/file/d/1LIkMVGFRjzN_IkHq7whMZ7kvvePBg1W0/view?usp=sharing"
+        className="btn btn-primary btn-block w-50"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Download my resume
+      </a>
 
-      <h4 className="my-3">
-        Technical Skills
-      </h4>
+      <h4 className="my-3">Technical Skills</h4>
 
       <article className="row row-cols-1 row-cols-md-3 justify-content-center">
         <div className="col mb-4">
@@ -19,7 +24,7 @@ export default function Resume() {
             <div className="card-body">
               <h5 className="card-title">Frontend Skills</h5>
               <p className="card-text">
-              JavaScript ES6+, HTML5, CSS3, JQuery, Bootstrap, React.js
+                JavaScript ES6+, HTML5, CSS3, JQuery, Bootstrap, React.js
               </p>
             </div>
           </div>
