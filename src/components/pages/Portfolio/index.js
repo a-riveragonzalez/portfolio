a0ac4d25281b7f0cb9004fb9720025ba3c4983c5
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-11-10 14:40:50 -0800

    edited styling on skills

--- src/components/pages/Portfolio/index.js
+++ src/components/pages/Portfolio/index.js
@@ -68,31 +68,225 @@ export default function Portfolio() {
       <div className="diagonal-line"> </div>
       <h2 className="mb-3 ">Projects</h2>
 
-      <Row xs={1} md={2} lg={3} className="g-4 card-container">
-        {projectArray.map((project) => (
-          <Col key={project.name}>
-            <Card >
-              <Card.Img variant="top" src={project.imgSrc} />
-              <Card.Body className="">
-                <Card.Title className="text-dark"> {project.name}</Card.Title>
-                <Card.Text className="text-dark">
-                  {project.description}
-                </Card.Text>
-                <Card.Text className="text-light">
-                  <a href={project.liveLink} className="btn btn-primary mx-3">
+      <div className="row row-cols-1 row-cols-md-2 row-cols-xl-3">
+        {/* *********** 1st item *********** */}
+        <div className="col mb-4">
+          <div className="card flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <img src={Ichooseyou} className="card-img-top" alt="..."></img>
+              </div>
+              <div className="flip-card-back">
+                <div className="card-body">
+                  <h5 className="card-title text-dark">I Choose You</h5>
+                  <p className="card-text text-dark">
+                    A responsive MERN dating web application that allows users
+                    to create Pokemon-themed profiles, take a quiz to determine
+                    their Pokemon, and match with other users to chat.
+                  </p>
+                  <a
+                    href="https://ichooseyou.herokuapp.com"
+                    className="btn btn-primary mx-3"
+                  >
                     {" "}
                     Demo{" "}
                   </a>
-                  <a href={project.githubLink} className="btn btn-primary">
+                  <a
+                    href="https://github.com/a-riveragonzalez/i-choose-you"
+                    className="btn btn-primary"
+                  >
                     {" "}
                     <i className="fa-solid fa-code"></i>
                   </a>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* *********** 2nd item *********** */}
+        <div className="col mb-4">
+          <div className="card flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <img src={Impulse} className="card-img-top" alt="..."></img>
+              </div>
+              <div className="flip-card-back">
+                <div className="card-body">
+                  <h5 className="card-title text-dark">Impulse: The Game</h5>
+                  <p className="card-text text-dark">
+                    A full stack browser-board-game where your character
+                    advances through game squares upon which they are compelled
+                    to Buy an Item or choose a Life Event Card that will impact
+                    your total Money and Mood Levels.
+                  </p>
+                  <a
+                    href="https://impulse-the-game.herokuapp.com/"
+                    className="btn btn-primary mx-3"
+                  >
+                    {" "}
+                    Demo{" "}
+                  </a>
+                  <a
+                    href="https://github.com/LujanSolo/Impulse"
+                    className="btn btn-primary"
+                  >
+                    {" "}
+                    <i className="fa-solid fa-code"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* *********** 3rd item *********** */}
+        <div className="col mb-4">
+          <div className="card flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <img
+                  src={EventFanFollow}
+                  className="card-img-top"
+                  alt="..."
+                ></img>
+              </div>
+              <div className="flip-card-back">
+                <div className="card-body">
+                  <h5 className="card-title text-dark">Event Fan Follow</h5>
+                  <p className="card-text text-dark">
+                    A web-based application that centers on retrieving and
+                    processing data from the API URLs, making it a user-friendly
+                    method to simultaneously display event ticket information
+                    combined with geolocation directly on the client side.
+                  </p>
+                  <a
+                    href="https://aasports89.github.io/Event-Fan-Follow/"
+                    className="btn btn-primary mx-3"
+                  >
+                    {" "}
+                    Demo{" "}
+                  </a>
+                  <a
+                    href="https://github.com/AASports89/Event-Fan-Follow"
+                    className="btn btn-primary"
+                  >
+                    {" "}
+                    <i className="fa-solid fa-code"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* *********** 4th item *********** */}
+        <div className="col mb-4">
+          <div className="card flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <img src={Techblog} className="card-img-top" alt="..."></img>
+              </div>
+              <div className="flip-card-back">
+                <div className="card-body">
+                  <h5 className="card-title text-dark">Tech Blog</h5>
+                  <p className="card-text text-dark">
+                    A web application, similar to a Wordpress site, where
+                    developers can publish their blog posts and comment on other
+                    developers posts as well.
+                  </p>
+                  <a
+                    href="https://tech-blog-6.herokuapp.com/"
+                    className="btn btn-primary mx-3"
+                  >
+                    {" "}
+                    Demo{" "}
+                  </a>
+                  <a
+                    href="https://github.com/a-riveragonzalez/tech-blog"
+                    className="btn btn-primary"
+                  >
+                    {" "}
+                    <i className="fa-solid fa-code"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* *********** 5th item *********** */}
+        <div className="col mb-4">
+          <div className="card flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <img
+                  src={WeatherDashboard}
+                  className="card-img-top"
+                  alt="..."
+                ></img>
+              </div>
+              <div className="flip-card-back">
+                <div className="card-body">
+                  <h5 className="card-title text-dark">Weather Dashboard</h5>
+                  <p className="card-text text-dark">
+                    A weather dashboard that allows users to search for a city
+                    and get its current weather and a five day forecast.
+                  </p>
+                  <a
+                    href="https://a-riveragonzalez.github.io/weather-dashboard/"
+                    className="btn btn-primary mx-3"
+                  >
+                    {" "}
+                    Demo{" "}
+                  </a>
+                  <a
+                    href="https://github.com/a-riveragonzalez/weather-dashboard"
+                    className="btn btn-primary"
+                  >
+                    {" "}
+                    <i className="fa-solid fa-code"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* *********** 6th item *********** */}
+        <div className="col mb-4">
+          <div className="card flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <img src={Notetaker} className="card-img-top" alt="..."></img>
+              </div>
+              <div className="flip-card-back">
+                <div className="card-body">
+                  <h5 className="card-title text-dark">Note Taker</h5>
+                  <p className="card-text text-dark">
+                    A web application that can be used to write, save, and
+                    delete notes.
+                  </p>
+                  <a
+                    href="https://rocky-headland-83783.herokuapp.com/"
+                    className="btn btn-primary mx-3"
+                  >
+                    {" "}
+                    Demo{" "}
+                  </a>
+                  <a
+                    href="https://github.com/a-riveragonzalez/note-taker"
+                    className="btn btn-primary"
+                  >
+                    {" "}
+                    <i className="fa-solid fa-code"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
