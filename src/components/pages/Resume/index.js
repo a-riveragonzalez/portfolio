b29ef42c222a40967bf74e0948b5516479628026
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2023-02-04 18:43:56 -0800

    added new resume link to resume section

--- src/components/pages/Resume/index.js
+++ src/components/pages/Resume/index.js
@@ -42,7 +42,7 @@ export default function Resume() {
           <div className="card h-100 w-50 inverted-2">
             <div className="card-body">
               <a
-                href="https://drive.google.com/uc?export=download&id=15kPb7zXmigpeC5YvAUCQeB3x9HJtbTF8"
+                href="https://drive.google.com/uc?export=download&id=1YRayVH1xbUSpUxQUwIlzLYL1kkFjLXsV"
                 className="btn btn-primary btn-block w-75"
                 target="_blank"
                 rel="noopener noreferrer"
