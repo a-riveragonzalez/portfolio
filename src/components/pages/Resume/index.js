1d92451b886b205011de7ebffb1a60ef6efbdeab
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2023-03-07 01:38:17 -0800

    edited resume section to display more skills

--- src/components/pages/Resume/index.js
+++ src/components/pages/Resume/index.js
@@ -6,14 +6,14 @@ export default function Resume() {
     <div className="resume text-center my-5" id="resume">
       <h1 className="mb-4">Resume</h1>
 
-      <article className="row row-cols-1 row-cols-md-3 justify-content-center ">
+      <article className="row row-cols-1 row-cols-md-2 justify-content-center ">
         <div className="col mb-4">
           <div className="card h-100 inverted-2">
             <div className="card-body ">
-              <h5 className="card-title ">Frontend Skills</h5>
+              <h5 className="card-title ">Languages</h5>
               <hr></hr>
               <p className="card-text ">
-                JavaScript ES6+, HTML5, CSS3, JQuery, Bootstrap, React.js
+              JavaScript ES6+ | HTML5 | CSS3
               </p>
             </div>
           </div>
@@ -22,11 +22,34 @@ export default function Resume() {
         <div className="col mb-4">
           <div className="card h-100 inverted-2">
             <div className="card-body">
-              <h5 className="card-title ">Backend Skills</h5>
+              <h5 className="card-title ">Frameworks</h5>
               <hr></hr>
               <p className="card-text ">
-                React, Node.js, Express, Redux, MySQL, MongoDB, NoSQL,
-                Handlebars
+              React.js | Node.js | Next.js | Express.js | Tailwind CSS | Redux | JQuery | Bootstrap | Handlebars
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col mb-4">
+          <div className="card h-100 inverted-2">
+            <div className="card-body">
+              <h5 className="card-title ">Databases</h5>
+              <hr></hr>
+              <p className="card-text ">
+              MySQL | NoSQL | MongoDB | GraphQL
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col mb-4">
+          <div className="card h-100 inverted-2">
+            <div className="card-body">
+              <h5 className="card-title ">Tools</h5>
+              <hr></hr>
+              <p className="card-text ">
+              Git | GitHub | Figma
               </p>
             </div>
           </div>
