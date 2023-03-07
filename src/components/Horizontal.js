036cf442f7f27fd4b141d088edbcb435f674f508
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2023-03-07 01:32:34 -0800

    added new icons in horizontal

--- src/components/Horizontal.js
+++ src/components/Horizontal.js
@@ -12,24 +12,33 @@ import NodePic from "../images/icons/node-js.png";
 import ReactPic from "../images/icons/react.png";
 import ReduxPic from "../images/icons/redux.png";
 import HandlebarsPic from "../images/icons/handlebars.png";
+import GitPic from "../images/icons/git.png";
+import GraphQLPic from "../images/icons/graphql.png";
+import NextjsPic from "../images/icons/nextjs.png";
+import TailwindPic from "../images/icons/tailwind.png";
 
 export default function Horizontal() {
   return (
-    <div className="horizontal-container row py-1">
+    <div className="horizontal-container row py-1 mx-auto justify-content-center">
       <img className="col-2 col-md-1 icon" src={HtmlPic}></img>
       <img className="col-2 col-md-1 icon" src={CssPic}></img>
       <img className="col-2 col-md-1 icon" src={JavaScriptPic}></img>
-      <img className="col-2 col-md-1 icon" src={JqueryPic}></img>
-
       <img className="col-2 col-md-1 icon" src={ReactPic}></img>
+
       <img className="col-2 col-md-1 icon" src={NodePic}></img>
       <img className="col-2 col-md-1 icon" src={ExpressPic}></img>
       <img className="col-2 col-md-1 icon" src={MongoPic}></img>
-
       <img className="col-2 col-md-1 icon" src={ReduxPic}></img>
+
       <img className="col-2 col-md-1 icon" src={MysqlPic}></img>
       <img className="col-2 col-md-1 icon" src={GithubPic}></img>
       <img className="col-2 col-md-1 icon" src={HandlebarsPic}></img>
+      <img className="col-2 col-md-1 icon" src={JqueryPic}></img>
+
+      <img className="col-2 col-md-1 icon" src={NextjsPic}></img>
+      <img className="col-2 col-md-1 icon" src={TailwindPic}></img>
+      <img className="col-2 col-md-1 icon" src={GitPic}></img>
+      <img className="col-2 col-md-1 icon" src={GraphQLPic}></img>
     </div>
   );
 }
